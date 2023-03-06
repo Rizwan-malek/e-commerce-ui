14e7a39109353575dcb3ec32dfcb80fc0966da19
Rizwan Malek <rmm@narola.email>
2023-03-06 16:56:42 +0530

    Route added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path='/home' element={<Home />} />
+          <Route path='/profile' element={<Home />} />
         </Routes>
       </BrowserRouter>
     </>
