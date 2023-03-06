8ae291ecb6de1823d1458a110350b96de4ae7ac7
Rizwan-malek <125858315+Rizwan-malek@users.noreply.github.com>
2023-03-06 16:58:42 +0530

    Changes done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 
 function App() {
-  console.log("Test")
+  console.log("Test - 1")
+   console.log("Test - 2")
   return (
     <>
       <BrowserRouter>
